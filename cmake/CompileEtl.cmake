7b44dce27f82a936f5c2a59ac5be21a6dc2d41aa
akowalew <ram.techen@gmail.com>
2018-03-01 18:01:47 +0100

    Corrected cmake build configuration

--- cmake/CompileEtl.cmake
+++ cmake/CompileEtl.cmake
@@ -10,18 +10,15 @@ file(GLOB etl_SOURCES
 message(STATUS "etl_SOURCES=${etl_SOURCES}")
 
 # Specify compile flags without exceptions and any unwind code
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
--std=gnu++17 \
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
 -Wall \
--Werror \
+-Wno-error\
 -Wextra \
 -ffunction-sections \
 -fdata-sections \
--fno-rtti \
 -fno-exceptions \
 -fno-unwind-tables \
 -fno-asynchronous-unwind-tables \
--fno-threadsafe-statics \
 ")
 
 # Remove unused sections from library
@@ -33,3 +30,6 @@ set(CMAKE_EXE_LINKER_FLAGS "\
 add_library(etl
 	${etl_SOURCES}
 )
+
+set_target_properties(etl PROPERTIES C_STANDARD 11)
+set_target_properties(etl PROPERTIES C_EXTENSIONS 11)
