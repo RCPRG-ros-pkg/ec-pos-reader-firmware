2580a9c6623081e498031e501a3d509d5f0c3185
akowalew <ram.techen@gmail.com>
2018-03-07 13:15:09 +0100

    abcc_drv_cfg.h - Added comments. Enabled IRQ pin interrupts.

--- include/abcc_adapt/abcc_drv_cfg.h
+++ include/abcc_adapt/abcc_drv_cfg.h
@@ -35,245 +35,68 @@
 #include "abcc_td.h"
 #include "abp.h"
 
-/*******************************************************************************
-** Constants
-********************************************************************************
-*/
+//! Enable only SPI driver
+#define ABCC_CFG_DRV_PARALLEL                      ( FALSE )
+#define ABCC_CFG_DRV_SPI                           ( TRUE )
+#define ABCC_CFG_DRV_SERIAL                        ( FALSE )
+#define ABCC_CFG_DRV_PARALLEL_30                   ( FALSE )
 
-/*------------------------------------------------------------------------------
-** All interface drivers are supported.
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_DRV_PARALLEL
-   #define ABCC_CFG_DRV_PARALLEL                      ( FALSE )
-#endif
-#ifndef ABCC_CFG_DRV_SPI
-   #define ABCC_CFG_DRV_SPI                           ( TRUE )
-#endif
-#ifndef ABCC_CFG_DRV_SERIAL
-   #define ABCC_CFG_DRV_SERIAL                        ( FALSE )
-#endif
-#ifndef ABCC_CFG_DRV_PARALLEL_30
-   #define ABCC_CFG_DRV_PARALLEL_30                   ( FALSE )
-#endif
+//! Disable getting or setting operating mode by switched.
+//! Instead fix operating mode to SPI
+#define ABCC_CFG_OP_MODE_GETTABLE                  ( FALSE )
+#define ABCC_CFG_OP_MODE_SETTABLE                  ( FALSE )
+#define ABCC_CFG_ABCC_OP_MODE_40 ABP_OP_MODE_SPI
 
-/*------------------------------------------------------------------------------
-** The software can both get the operating mode from external resource and set
-** the selected operating mode to the ABCC host connector.
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_OP_MODE_GETTABLE
-   #define ABCC_CFG_OP_MODE_GETTABLE                  ( FALSE )
-#endif
-#ifndef ABCC_CFG_OP_MODE_SETTABLE
-   #define ABCC_CFG_OP_MODE_SETTABLE                  ( FALSE )
-#endif
-#if( !ABCC_CFG_OP_MODE_GETTABLE )
-   // #ifndef ABCC_CFG_ABCC_OP_MODE_30
-   //    #define ABCC_CFG_ABCC_OP_MODE_30 ABP_OP_MODE_SPI
-   // #endif
-   #ifndef ABCC_CFG_ABCC_OP_MODE_40
-      #define ABCC_CFG_ABCC_OP_MODE_40 ABP_OP_MODE_SPI
-   #endif
-#endif
+//! Configure SPI message fragment length.
+//! Most of small messages are under 32 octets
+#define ABCC_CFG_SPI_MSG_FRAG_LEN                  ( 32 )
 
-/*------------------------------------------------------------------------------
-** SPI frame configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#if( ABCC_CFG_DRV_SPI )
-   #ifndef ABCC_CFG_SPI_MSG_FRAG_LEN
-      #define ABCC_CFG_SPI_MSG_FRAG_LEN                  ( 32 )
-   #endif
-#endif
+//! Enable module ID checking from MI0 and MI1 pins
+#define ABCC_CFG_MODULE_ID_PINS_CONN               ( TRUE )
 
-/*------------------------------------------------------------------------------
-** ABCC memory access configuration for parallel operating modes
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_MEMORY_MAPPED_ACCESS
-   #define ABCC_CFG_MEMORY_MAPPED_ACCESS              ( FALSE )
-#endif
-#if( ABCC_CFG_MEMORY_MAPPED_ACCESS )
-   #ifndef ABCC_CFG_PARALLEL_BASE_ADR
-      #define ABCC_CFG_PARALLEL_BASE_ADR                 ( 0x00000000 )
-   #endif
-#endif
+//! Enable module detection checking from MD0 pin
+#define ABCC_CFG_MOD_DETECT_PINS_CONN              ( TRUE )
 
-/*------------------------------------------------------------------------------
-** If Module ID pins are connected no soft configuration is required.
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_MODULE_ID_PINS_CONN
-   #define ABCC_CFG_MODULE_ID_PINS_CONN               ( TRUE )
-#endif
+//! Configure message handling internals
+#define ABCC_CFG_MAX_NUM_APPL_CMDS                 ( 2 )
+#define ABCC_CFG_MAX_NUM_ABCC_CMDS                 ( 2 )
+#define ABCC_CFG_MAX_MSG_SIZE                      ( 255 )
+#define ABCC_CFG_MAX_PROCESS_DATA_SIZE             ( 512 )
 
-#if( !ABCC_CFG_MODULE_ID_PINS_CONN )
-   #ifndef ABCC_CFG_ABCC_MODULE_ID
-      #define ABCC_CFG_ABCC_MODULE_ID     ABP_MODULE_ID_ACTIVE_ABCC40
-   #endif
-#endif
+//! Disable both SYNC and usage of SYNC signal
+#define ABCC_CFG_SYNC_ENABLE                    ( FALSE )
+#define ABCC_CFG_USE_ABCC_SYNC_SIGNAL           ( FALSE )
 
-/*------------------------------------------------------------------------------
-** Module detection configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_MOD_DETECT_PINS_CONN
-   #define ABCC_CFG_MOD_DETECT_PINS_CONN              ( TRUE )
-#endif
+//! Enable interrupts from IRQ pin
+#define ABCC_CFG_POLL_ABCC_IRQ_PIN                 ( FALSE )
+#define ABCC_CFG_INT_ENABLED                       ( TRUE )
+// #define ABCC_CFG_INT_ENABLE_MASK_SPI            ( 0 )
 
-/*------------------------------------------------------------------------------
-** Configuration of message handling
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_MAX_NUM_APPL_CMDS
-   #define ABCC_CFG_MAX_NUM_APPL_CMDS                 ( 2 )
-#endif
-#ifndef ABCC_CFG_MAX_NUM_ABCC_CMDS
-   #define ABCC_CFG_MAX_NUM_ABCC_CMDS                 ( 2 )
-#endif
-#ifndef ABCC_CFG_MAX_MSG_SIZE
-   #define ABCC_CFG_MAX_MSG_SIZE                      ( 255 )
-#endif
-#ifndef ABCC_CFG_MAX_PROCESS_DATA_SIZE
-   #define ABCC_CFG_MAX_PROCESS_DATA_SIZE             ( 512 )
-#endif
+//! Configure anybus watchdog to be called every 1 second
+#define ABCC_CFG_WD_TIMEOUT_MS                     ( 1000 )
 
-/*------------------------------------------------------------------------------
-** Sync configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_SYNC_ENABLE
-   #define ABCC_CFG_SYNC_ENABLE                       ( FALSE )
-#endif
-#if( ABCC_CFG_SYNC_ENABLE )
-   #ifndef ABCC_CFG_USE_ABCC_SYNC_SIGNAL
-      #define ABCC_CFG_USE_ABCC_SYNC_SIGNAL           ( TRUE )
-   #endif
-#endif
+//! Disable remapping
+#define ABCC_CFG_REMAP_SUPPORT_ENABLED             ( FALSE )
 
-/*------------------------------------------------------------------------------
-** Interrupt configuration excluding sync
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_POLL_ABCC_IRQ_PIN
-   #define ABCC_CFG_POLL_ABCC_IRQ_PIN                 ( FALSE )
-#endif
-#ifndef ABCC_CFG_INT_ENABLED
-   #define ABCC_CFG_INT_ENABLED                       ( FALSE )
-#endif
-#if( ABCC_CFG_INT_ENABLED )
-   #ifndef ABCC_CFG_INT_ENABLE_MASK_PAR
-      #define ABCC_CFG_INT_ENABLE_MASK_PAR            ( ABP_INTMASK_RDPDIEN | ABP_INTMASK_STATUSIEN | ABP_INTMASK_RDMSGIEN | ABP_INTMASK_WRMSGIEN | ABP_INTMASK_ANBRIEN  )
-   #endif
-   #ifndef ABCC_CFG_INT_ENABLE_MASK_SPI
-      #define ABCC_CFG_INT_ENABLE_MASK_SPI            ( 0 )
-   #endif
-   #ifndef ABCC_CFG_HANDLE_INT_IN_ISR_MASK
-      #define ABCC_CFG_HANDLE_INT_IN_ISR_MASK         ( ABP_INTMASK_RDPDIEN )
-   #endif
-#endif
+//! Configuration of ADI
+#define ABCC_CFG_STRUCT_DATA_TYPE                  ( FALSE )
+#define ABCC_CFG_ADI_GET_SET_CALLBACK              ( FALSE )
+#define ABCC_CFG_64BIT_ADI_SUPPORT                 ( FALSE )
 
-/*------------------------------------------------------------------------------
-** Watchdog configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_WD_TIMEOUT_MS
-   #define ABCC_CFG_WD_TIMEOUT_MS                     ( 1000 )
-#endif
+//! Command sequencer configuration
+#define ABCC_CFG_MAX_NUM_CMD_SEQ                      ( 2 )
+#define ABCC_CFG_CMD_SEQ_MAX_NUM_RETRIES              ( 0 )
 
-/*------------------------------------------------------------------------------
-** Remap support configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_REMAP_SUPPORT_ENABLED
-   #define ABCC_CFG_REMAP_SUPPORT_ENABLED             ( FALSE )
-#endif
-/*------------------------------------------------------------------------------
-** The Application data object ADI support configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_STRUCT_DATA_TYPE
-   #define ABCC_CFG_STRUCT_DATA_TYPE                  ( FALSE )
-#endif
-#ifndef ABCC_CFG_ADI_GET_SET_CALLBACK
-   #define ABCC_CFG_ADI_GET_SET_CALLBACK              ( FALSE )
-#endif
-#ifndef ABCC_CFG_64BIT_ADI_SUPPORT
-   #define ABCC_CFG_64BIT_ADI_SUPPORT                 ( FALSE )
-#endif
+//! Enable all debug prints
+#define ABCC_CFG_ERR_REPORTING_ENABLED             ( TRUE )
+#define ABCC_CFG_DEBUG_EVENT_ENABLED               ( TRUE )
+#define ABCC_CFG_DEBUG_ERR_ENABLED                 ( TRUE )
+#define ABCC_CFG_DEBUG_MESSAGING                   ( TRUE )
+#define ABCC_CFG_DEBUG_CMD_SEQ_ENABLED             ( TRUE )
 
-/*------------------------------------------------------------------------------
-** Command sequencer configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_MAX_NUM_CMD_SEQ
-   #define ABCC_CFG_MAX_NUM_CMD_SEQ                      ( 2 )
-#endif
-#ifndef ABCC_CFG_CMD_SEQ_MAX_NUM_RETRIES
-   #define ABCC_CFG_CMD_SEQ_MAX_NUM_RETRIES              ( 0 )
-#endif
-
-/*------------------------------------------------------------------------------
-** Debug and error macro configuration
-**
-** Check the descriptions in "./abcc_drv/inc/abcc_cfg.h" for more information
-** about the purpose of each separate 'define'.
-**------------------------------------------------------------------------------
-*/
-#ifndef ABCC_CFG_ERR_REPORTING_ENABLED
-   #define ABCC_CFG_ERR_REPORTING_ENABLED             ( TRUE )
-#endif
-#ifndef ABCC_CFG_DEBUG_EVENT_ENABLED
-   #define ABCC_CFG_DEBUG_EVENT_ENABLED               ( TRUE )
-#endif
-#ifndef ABCC_CFG_DEBUG_ERR_ENABLED
-   #define ABCC_CFG_DEBUG_ERR_ENABLED                 ( TRUE )
-#endif
-#ifndef ABCC_CFG_DEBUG_MESSAGING
-   #define ABCC_CFG_DEBUG_MESSAGING                   ( TRUE )
-#endif
-#ifndef ABCC_CFG_DEBUG_CMD_SEQ_ENABLED
-   #define ABCC_CFG_DEBUG_CMD_SEQ_ENABLED             ( TRUE )
-#endif
+//! Set startup timeout to be 1.5 seconds.
+//! After that, if driver is still not responding, APPL_MODULE_NOT_ANSWERING
+//! will be returned
+#define ABCC_CFG_STARTUP_TIME_MS           ( 1500 )
 
 #endif  /* inclusion lock */
