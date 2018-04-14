3c417f000c63f5c8ce0f9c05a19138ca909c7292
akowalew <ram.techen@gmail.com>
2018-04-14 17:49:21 +0200

    Removed internal loop inside Encoders. Leaved only blocking call 'readPositions'

--- include/app/encoders/Encoders.hpp
+++ include/app/encoders/Encoders.hpp
@@ -2,11 +2,7 @@
 
 #include "device/SSIMaster.hpp"
 
-#include "hohner/SMRS59.hpp"
-
-#include "app/common/SysTickDriver.hpp"
-
-#include "app/ModuleState.hpp"
+#include "component/SSIEncoder.hpp"
 
 namespace app {
 namespace encoders {
@@ -14,68 +10,20 @@ namespace encoders {
 class Encoders
 {
 public:
-	Encoders(common::SysTickDriver& sysTickDriver);
+	Encoders();
 
-	//! Starts module
-	void start();
-
-	//! Stops module
-	void stop();
-
-	//! Checks, if module is in active state
-	bool isActive() const
-	{
-		return _moduleState == ModuleState::Active;
-	}
-
-	//! Checks, if module is in failed state
-	bool isFailed() const
-	{
-		return _moduleState == ModuleState::Error;
-	}
-
-	//! Checks, if module is in ready state
-	bool isReady() const
-	{
-		return _moduleState == ModuleState::Ready;
-	}
-
-	ModuleState getState() const
-	{
-		return _moduleState;
-	}
+	int readPosition();
 
 private:
-	using ErrorStatus = embxx::error::ErrorStatus;
-	using ErrorCode = ErrorStatus::ErrorCodeType;
-
-	template<typename T>
-	using StaticFunction = embxx::util::StaticFunction<T>;
-
-	using ErrorCallback = StaticFunction<void(const ErrorStatus&)>;
-
-	using Timer = common::SysTickDriver::Timer;
-
 	constexpr static auto BitRate = 1250000;
 	constexpr static auto DataWidth = 13;
 	using SSIMasterDevice = device::SSIMaster0<BitRate, DataWidth>;
 
 	constexpr static auto Resolution = DataWidth;
-	using Encoder = hohner::SMRS59<SSIMasterDevice, Resolution>;
+	using SSIEncoder = component::SSIEncoder<SSIMasterDevice, Resolution>;
 
-	void doStart();
-
-	void execWait();
-
-	void readPosition();
-
-	Timer _timer;
 	SSIMasterDevice _ssiMasterDevice;
-	Encoder _encoder;
-	hohner::Position _position;
-	ErrorCallback _errorCallback;
-	ModuleState _moduleState = ModuleState::Ready;
-	bool _running = false;
+	SSIEncoder _ssiEncoder;
 };
 
 } // namespace encoders
--- src/app/encoders/Encoders.cpp
+++ src/app/encoders/Encoders.cpp
@@ -7,87 +7,17 @@
 namespace app {
 namespace encoders {
 
-Encoders::Encoders(common::SysTickDriver& sysTickDriver)
-	:	_timer(sysTickDriver.allocTimer()),
-		_ssiMasterDevice(),
-		_encoder(_ssiMasterDevice)
+Encoders::Encoders()
+	:	_ssiMasterDevice(),
+		_ssiEncoder(_ssiMasterDevice)
 {
-	assert(_timer.isValid());
-
 	UARTprintf("[Encoders] ready\n");
-	assert(_moduleState == ModuleState::Ready);
 }
 
-void
-Encoders::start()
-{
-	if(_moduleState == ModuleState::Active)
-	{
-		return; // nothing to do
-	}
-
-	UARTprintf("[Encoders] starting...\n");
-
-	_moduleState = ModuleState::Active;
-	_running = true;
-	execWait();
-
-	UARTprintf("[Encoders] started\n");
-	assert(_moduleState == ModuleState::Active);
-	assert(_running);
-}
-
-void
-Encoders::stop()
-{
-	if(_moduleState != ModuleState::Active)
-	{
-		return;
-	}
-
-	UARTprintf("[Encoders] stopping...\n");
-
-	_running = false; // signal stop request
-
-	assert(!_running);
-}
-
-void
-Encoders::execWait()
-{
-	assert(_moduleState == ModuleState::Active);
-
-	_timer.asyncWait(std::chrono::milliseconds(1),
-		[this](const auto& errorStatus)
-		{
-			assert(!errorStatus); // no errors will occur
-			static_cast<void>(errorStatus);
-
-			if(_running)
-			{
-				// keep reading
-				readPosition();
-				execWait();
-			}
-			else
-			{
-				// stop request signalled
-				_moduleState = ModuleState::Ready;
-				UARTprintf("[Encoders] stopped\n");
-			}
-		});
-}
-
-void
+int
 Encoders::readPosition()
 {
-	const auto position = _encoder.readPosition();
-	if(position != _position)
-	{
-		_position = position;
-		UARTprintf("[Encoders] position changed: %d\n",
-			_position.value());
-	}
+	return _ssiEncoder.readPosition();
 }
 
 } // namespace encoders
