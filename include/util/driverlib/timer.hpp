74be8ff443a6a989ccae3ca470e8cae1492bc46f
akowalew <ram.techen@gmail.com>
2018-04-14 17:40:28 +0200

    Added utilities to TI driverlib's Timer functions

---
+++ include/util/driverlib/timer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include "tivaware/inc/hw_memmap.h"
+#include "tivaware/inc/hw_timer.h"
+#include "tivaware/inc/hw_types.h"
+#include "tivaware/driverlib/timer.h"
+#include "tivaware/driverlib/rom.h"
+#include "tivaware/driverlib/rom_map.h"
+
+bool TimerIsEnabled(std::uint32_t baseAddress, std::uint32_t timer);
+std::uint32_t TimerMaskedIntStatus(std::uint32_t baseAddress);
+
+inline bool
+TimerIsEnabled(std::uint32_t baseAddress, std::uint32_t timer)
+{
+	assert((timer == TIMER_A)
+		|| (timer == TIMER_B)
+		|| (timer == TIMER_BOTH));
+
+	return (HWREG(baseAddress + TIMER_O_CTL) & timer);
+}
+
+inline std::uint32_t
+TimerMaskedIntStatus(std::uint32_t baseAddress)
+{
+	return HWREG(baseAddress + TIMER_O_MIS);
+}
