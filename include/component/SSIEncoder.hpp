3187476e6f1d9af832b75ff1c914d1f465d94313
akowalew <ram.techen@gmail.com>
2018-04-18 00:06:48 +0200

    Updated SSIEncoder definition according to new body of SSIMaster device

--- include/component/SSIEncoder.hpp
+++ include/component/SSIEncoder.hpp
@@ -4,27 +4,45 @@
 
 namespace component {
 
-template<typename TSSIMasterDevice, std::size_t Resolution>
+template<typename TSSIMasterDevice>
 class SSIEncoder
 {
 public:
+	using PositionType = int;
+
+	constexpr static auto MinResolution = TSSIMasterDevice::MinDataWidth;
+	constexpr static auto MaxResolution = TSSIMasterDevice::MaxDataWidth;
+
+	using SSIMasterDevice = TSSIMasterDevice;
+
 	//! Constructor
-	SSIEncoder(TSSIMasterDevice& ssiMasterDevice)
+	SSIEncoder(SSIMasterDevice& ssiMasterDevice,
+		std::size_t resolution)
 		:	_ssiMasterDevice(ssiMasterDevice)
 	{
+		assert(resolution >= MinResolution
+			&& resolution <= MaxResolution);
+		_ssiMasterDevice.setDataWidth(resolution);
+	}
 
+	//! Constructor
+	SSIEncoder(SSIMasterDevice& ssiMasterDevice,
+		std::size_t resolution, int bitRate)
+		:	SSIEncoder(ssiMasterDevice, resolution)
+	{
+		setBitRate(bitRate);
 	}
 
 	//! Reads encoder position value. Blocking call.
-	int readPosition()
+	PositionType readPosition()
 	{
-		// read one frame from device
+		// read one data item from device
 		assert(!_ssiMasterDevice.isBusy());
-		auto data = _ssiMasterDevice.read();
+		auto data = _ssiMasterDevice.readOne();
 
 		// clear unused bits
-		assert(_ssiMasterDevice.getFrameWidth() == Resolution);
-		data &= ((1 << Resolution) - 1);
+		const auto resolution = _ssiMasterDevice.getDataWidth();
+		data &= ((1 << resolution) - 1);
 
 		// convert from gray code to binary
 		const auto value = etl::gray_to_binary(data);
@@ -32,8 +50,26 @@ public:
 		return value;
 	}
 
+	//! Sets bit rate of transmission with encoder
+	void setBitRate(int bitRate)
+	{
+		_ssiMasterDevice.setBitRate(bitRate);
+	}
+
+	//! Gets the bit rate of transmission with encoder
+	int getBitRate() const
+	{
+		return _ssiMasterDevice.getBitRate();
+	}
+
+	//! Gets the resolution of encoder
+	std::size_t getResolution() const
+	{
+		return _ssiMasterDevice.getDataWidth();
+	}
+
 private:
-	TSSIMasterDevice& _ssiMasterDevice;
+	SSIMasterDevice& _ssiMasterDevice;
 };
 
 } // namespace component
