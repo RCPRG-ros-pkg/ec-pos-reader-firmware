c916cebbb1739ec7c908dd2770b8c7058f372652
akowalew <ram.techen@gmail.com>
2018-04-18 00:05:26 +0200

    Written complete implementation of SSIMaster device

--- include/device/SSIMaster.hpp
+++ include/device/SSIMaster.hpp
@@ -1,137 +1,112 @@
 #pragma once
 
-#include <cassert>
 #include <cstdint>
+#include <type_traits>
 #include <limits>
-
-#include "tivaware/driverlib/sysctl.h"
 
 #include "util/driverlib/ssi.hpp"
 
+#include "init.hpp"
+
 #include "device/Peripheral.hpp"
 
-extern int clockHz;
+#include "array_view.h"
 
 namespace device {
 
 class SSIMasterBase
 {
-public:
-	//! Minimum frame width
-	static constexpr std::size_t MinFrameWidth = 4;
-
-	//! Maximum frame width
-	static constexpr std::size_t MaxFrameWidth = 16;
-
-	//! Data type, which can contain one data frame. Used in read operations
-	using DataType = std::uint16_t;
-	static_assert(std::numeric_limits<DataType>::digits > MinFrameWidth);
-	static_assert(std::numeric_limits<DataType>::digits <= MaxFrameWidth);
-
-	//! Blocking reads a bunch of data from Slave device.
-	void read(DataType* buffer, std::size_t size, std::size_t n);
-
-	//! Blocking reads single data element from Slave device.
-	DataType read();
-
-	//! Returns bit rate, at which device is currently running
-	int getBitRate() const;
-
-	//! Returns data width, with which device is currently running
-	std::size_t getFrameWidth() const;
-
-	//! Checks, if device is currently transfering.
-	bool isBusy() const;
-
 protected:
 	//! Constructor
-	explicit SSIMasterBase(std::uint32_t baseAddress,
-		int bitRate, std::size_t frameWidth);
+	SSIMasterBase(std::uint32_t baseAddress,
+		int bitRate, std::size_t dataWidth);
+
+	//! Constructor
+	explicit
+	SSIMasterBase(std::uint32_t baseAddress);
+
+public:
+	static constexpr std::size_t MinDataWidth = SSI_MIN_DATA_WIDTH;
+	static constexpr std::size_t MaxDataWidth = SSI_MAX_DATA_WIDTH;
+	static_assert(MinDataWidth < MaxDataWidth);
+
+	using DataType = SSIDataType;
+	static_assert(std::numeric_limits<DataType>::digits >= MaxDataWidth,
+		"Underlying data type must have at least MaxDataWidth bits size");
+
+	//! Reads one data item from SSI slave in blocking way
+	DataType readOne();
+
+	//! Reads multiple data from SSI slave in blocking way
+	void read(etl::array_view<DataType> buffer, std::size_t n);
+
+	//! Sets bit rate of transmission with SSI slave
+	void setBitRate(int bitRate)
+	{
+		SSIBitRateSet(_baseAddress, ClockHz, bitRate);
+	}
+
+	//! Gets bit rate of transmission with SSI slave
+	int getBitRate() const
+	{
+		return SSIBitRateGet(_baseAddress, ClockHz);
+	}
+
+	//! Sets data width in transmission with SSI slave
+	void setDataWidth(std::size_t dataWidth)
+	{
+		SSIDataWidthSet(_baseAddress, dataWidth);
+	}
+
+	//! Gets data width in transmission with SSI slave
+	std::size_t getDataWidth() const
+	{
+		return SSIDataWidthGet(_baseAddress);
+	}
+
+	//! Determines, whether SSI master is currently communicating with SSI slave
+	bool isBusy() const
+	{
+		return SSIBusy(_baseAddress);
+	}
 
 private:
-	//! Configures device to work as master with specified attributes
-	void configure(int bitRate, std::size_t dataWidth);
+	constexpr static auto DefaultBitRate = 1000000;
 
-	//! Size of Rx and Tx FIFO for SSI (both are equal)
-	static constexpr std::size_t FIFOSize = 8;
+	constexpr static auto DefaultDataWidth = 8;
 
-	//! Dummy data, which will be used during writing to SSI
-	static constexpr SSIMasterBase::DataType DummyData = 0;
+	constexpr static auto DummyData = DataType();
 
-	//! Base address of Peripheral SSI module
 	const std::uint32_t _baseAddress;
 };
 
-/**
- * @brief Returns bit rate, at which SSI device is currently running
- * @details
- */
-inline int
-SSIMasterBase::getBitRate() const
-{
-	return SSIGetBitRate(_baseAddress, clockHz);
-}
-
-/**
- * @brief Checks, if device is currently transferring data
- * @details
- */
-inline bool
-SSIMasterBase::isBusy() const
-{
-	return SSIBusy(_baseAddress);
-}
-
-/**
- * @brief Returns data width, with which device is currently running
- * @details [long description]
- * @post Returned frame frame will be in range [`MinFrameWidth`, `MaxFrameWidth`]
- * @return frame width
- */
-inline std::size_t
-SSIMasterBase::getFrameWidth() const
-{
-	const auto frameWidth = SSIGetDataWidth(_baseAddress);
-	assert(frameWidth >= MinFrameWidth
-		&& frameWidth <= MaxFrameWidth);
-	return frameWidth;
-}
-
-template<std::uint32_t TPeripheralID, std::uint32_t TBaseAddress,
-	int TBitRate, std::size_t TFrameWidth>
+template<std::uint32_t TBaseAddress, std::uint32_t TPeripheralId>
 class SSIMaster
-	:	public Peripheral<TPeripheralID>,
-		public SSIMasterBase
+	:	public SSIMasterBase,
+		public Peripheral<TPeripheralId>
 {
 public:
-	using PeripheralType = Peripheral<TPeripheralID>;
+	static_assert(TBaseAddress != 0);
+	constexpr static auto BaseAddress = TBaseAddress;
 
-	static_assert(TBaseAddress > 0);
-	constexpr static std::uint32_t BaseAddress = TBaseAddress;
-
-	static_assert(TBitRate > 0);
-	constexpr static int BitRate = TBitRate;
-
-	static_assert(TFrameWidth >= MinFrameWidth);
-	static_assert(TFrameWidth <= MaxFrameWidth);
-	constexpr static std::size_t FrameWidth = TFrameWidth;
+	using PeripheralType = Peripheral<TPeripheralId>;
 
 	SSIMaster()
-		:	PeripheralType::Peripheral(),
-			SSIMasterBase(BaseAddress, BitRate, FrameWidth)
-	{}
+		:	SSIMasterBase(BaseAddress),
+			PeripheralType::Peripheral()
+	{
+
+	}
+
+	SSIMaster(int bitRate, std::size_t dataWidth)
+		:	SSIMasterBase(BaseAddress, bitRate, dataWidth),
+			PeripheralType::Peripheral()
+	{
+
+	}
 };
 
-template<int TBitRate, std::size_t TFrameWidth>
-using SSIMaster0 = SSIMaster<SYSCTL_PERIPH_SSI0, SSI0_BASE, TBitRate, TFrameWidth>;
-
-template<int TBitRate, std::size_t TFrameWidth>
-using SSIMaster1 = SSIMaster<SYSCTL_PERIPH_SSI1, SSI1_BASE, TBitRate, TFrameWidth>;
-
-template<int TBitRate, std::size_t TFrameWidth>
-using SSIMaster2 = SSIMaster<SYSCTL_PERIPH_SSI2, SSI2_BASE, TBitRate, TFrameWidth>;
-
-template<int TBitRate, std::size_t TFrameWidth>
-using SSIMaster3 = SSIMaster<SYSCTL_PERIPH_SSI3, SSI3_BASE, TBitRate, TFrameWidth>;
-
 } // namespace device
+
+
+
--- src/device/SSIMaster.cpp
+++ src/device/SSIMaster.cpp
@@ -1,100 +1,139 @@
 #include "device/SSIMaster.hpp"
 
 #include <algorithm>
-#include <limits>
-
-#include "init.hpp"
 
 namespace device {
 
 /**
  * @brief Constructor
- * @details Configures device to work at specified bit rate
- * Preconditions:
- * - SSI module must be configured to work under Master mode
- * - Data width of module must be same as DataWidth static member
+ * @details [long description]
  *
- * @param baseAddress base address of SSI module
+ * @param ssi [description]
+ * @param bitRate [description]
+ * @param dataWidth [description]
  */
 SSIMasterBase::SSIMasterBase(std::uint32_t baseAddress,
-	int bitRate, std::size_t frameWidth)
+	int bitRate, std::size_t dataWidth)
 	:	_baseAddress(baseAddress)
 {
-	assert(bitRate > 0);
-	assert(frameWidth >= MinFrameWidth
-		&& frameWidth <= MaxFrameWidth);
-
-	configure(bitRate, frameWidth);
+	SSIConfigSetExpClk(_baseAddress,
+		ClockHz,
+		SSI_FRF_TI,
+		SSI_MODE_MASTER,
+		bitRate,
+		dataWidth);
 }
 
-void
-SSIMasterBase::read(DataType* buffer, std::size_t size, std::size_t n)
+/**
+ * @brief Constructor
+ * @details [long description]
+ *
+ * @param ssi [description]
+ * @param e [description]
+ * @param h [description]
+ */
+SSIMasterBase::SSIMasterBase(std::uint32_t baseAddress)
+	:	SSIMasterBase(baseAddress, DefaultBitRate, DefaultDataWidth)
 {
-	// Data type must be able to contain at least one frame
-	constexpr auto DataTypeResolution = std::numeric_limits<DataType>::digits;
-	static_assert(DataTypeResolution >= MaxFrameWidth);
 
-	assert(buffer != nullptr);
-	assert(size >= n);
-	assert(size <= FIFOSize); // implemented only for buffers max to FIFO capacity
-	assert(SSIEnabled(_baseAddress));
-	assert(SSIIdle(_baseAddress));
+}
+
+/**
+ * @brief Reads one data item from SSI slave in blocking way
+ * @details [long description]
+ * @return [description]
+ */
+SSIMasterBase::DataType
+SSIMasterBase::readOne()
+{
+	assert(!SSIEnabled(_baseAddress));
 	assert(SSIRxEmpty(_baseAddress));
+	assert(SSITxEmpty(_baseAddress));
 
-	for(int i = 0; i < static_cast<int>(n); ++i)
+	// insert one dummy data item into Tx FIFO
+	SSIDataPutNow(_baseAddress, DummyData);
+
+	// start transmission of CLK signal and receive data from slave
+	SSIEnable(_baseAddress);
+
+	// wait for one data item to be received
+	DataType data;
+	SSIDataGet(_baseAddress, &data);
+
+	// end transmission
+	SSIDisable(_baseAddress);
+
+	return data;
+}
+
+/**
+ * @brief Reads multiple data from SSI slave in blocking way
+ * @details [long description]
+ *
+ * @param buffer [description]
+ * @param size [description]
+ * @param n [description]
+ */
+void
+SSIMasterBase::read(etl::array_view<DataType> buffer, std::size_t n)
+{
+	assert(buffer != nullptr);
+	assert(n > 0);
+	assert(!SSIEnabled(_baseAddress));
+	assert(SSIRxEmpty(_baseAddress));
+	assert(SSITxEmpty(_baseAddress));
+
+	if(n <= SSI_FIFO_SIZE)
 	{
-		assert(SSITxNotFull(_baseAddress));
-		SSIPutDataNow(_baseAddress, DummyData);
+		// there are as much data to receive as FIFO capacity
+		// simply fill Tx FIFO and wait for Rx data
+
+		// fill Tx FIFO first with dummy chars
+		for(unsigned i = 0; i < n; ++i)
+		{
+			SSIDataPutNow(_baseAddress, DummyData);
+		}
+
+		// start transmission of CLK signal and receive data from slave
+		SSIEnable(_baseAddress);
+
+		// generate Rx data
+		for(auto& data : buffer)
+		{
+			SSIDataGet(_baseAddress, &data);
+		}
+	}
+	else
+	{
+		// there are more data to receive than FIFO capacity
+		// Tx FIFO must be complemented during receive operations
+
+		// fill entire Tx FIFO first with dummy chars
+		for(unsigned i = 0; i < SSI_FIFO_SIZE; ++i)
+		{
+			SSIDataPutNow(_baseAddress, DummyData);
+		}
+
+		// start transmission of CLK signal and receive data from slave
+		SSIEnable(_baseAddress);
+
+		// generate Rx data
+		auto elapsed = n - SSI_FIFO_SIZE;
+		for(auto& data : buffer)
+		{
+			// wait for one data item to be received
+			SSIDataGet(_baseAddress, &data);
+
+			// queue elapsed CLK transmissions
+			if(elapsed--)
+			{
+				SSIDataPutNow(_baseAddress, DummyData);
+			}
+		}
 	}
 
-	const auto begin = buffer;
-	const auto end = buffer + size;
-	std::generate(begin, end, [this]() {
-		assert(SSIRxNotEmpty(_baseAddress));
-		const auto data = SSIGetDataNow(_baseAddress);
-		return static_cast<DataType>(data);
-	});
-}
-
-SSIMasterBase::DataType
-SSIMasterBase::read()
-{
-	// Data type must be able to contain at least one frame
-	constexpr auto DataTypeResolution = std::numeric_limits<DataType>::digits;
-	static_assert(DataTypeResolution >= MaxFrameWidth);
-
-	assert(SSIEnabled(_baseAddress));
-	assert(SSIIdle(_baseAddress));
-	assert(SSIRxEmpty(_baseAddress));
-
-	// insert Dummy data to init transfer
-	assert(SSITxNotFull(_baseAddress));
-	SSIPutDataNow(_baseAddress, DummyData);
-
-	// wait for transfer to be completed
-	while(SSIBusy(_baseAddress));
-
-	// data should be available
-	assert(SSIRxNotEmpty(_baseAddress));
-	const auto data = SSIGetDataNow(_baseAddress);
-
-	return static_cast<DataType>(data);
-}
-
-void
-SSIMasterBase::configure(int bitRate, std::size_t frameWidth)
-{
-	assert(bitRate != 0);
-	assert(frameWidth >= MinFrameWidth
-		&& frameWidth <= MaxFrameWidth);
-
-	SSIConfigSetExpClk(_baseAddress, ClockHz, SSI_FRF_TI,
-		SSI_MODE_MASTER, bitRate, frameWidth);
-	SSIEnable(_baseAddress);
-	assert(SSIGetMode(_baseAddress) == SSIMode::Master);
-	assert(SSIGetDataWidth(_baseAddress) == frameWidth);
+	// stop transmission
+	SSIDisable(_baseAddress);
 }
 
 } // namespace device
-
-
