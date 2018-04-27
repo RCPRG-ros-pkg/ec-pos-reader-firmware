2c546efbf211f95e806f92d03ffc085f0e68701b
akowalew <ram.techen@gmail.com>
2018-04-27 15:36:13 +0200

    Added new extensions to TI driverlib's interrupt module

--- include/util/driverlib/interrupt.hpp
+++ include/util/driverlib/interrupt.hpp
@@ -6,9 +6,14 @@
 #include "tivaware/inc/hw_types.h"
 #include "tivaware/inc/hw_memmap.h"
 #include "tivaware/inc/hw_ints.h"
+#include "tivaware/inc/hw_nvic.h"
 #include "tivaware/driverlib/interrupt.h"
 #include "tivaware/driverlib/debug.h"
 
+void IntGeneralEnable(uint32_t ui32Interrupt);
+void IntGeneralDisable(uint32_t ui32Interrupt);
+bool IntGeneralEnabledGet(uint32_t ui32Interrupt);
+bool IntGeneralPendGet(uint32_t ui32Interrupt);
 void IntUserDataSet(uint32_t ui32Interrupt, void* pvUserData);
 void IntUserDataUnset(uint32_t ui32Interrupt);
 void* IntUserDataGet(uint32_t ui32Interrupt);
@@ -39,4 +44,44 @@ IntUserDataGet(uint32_t ui32Interrupt)
 	extern void* g_pvUserDataArray[NUM_INTERRUPTS];
 	void* pvUserData = g_pvUserDataArray[ui32Interrupt];
 	return pvUserData;
+}
+
+inline void
+IntGeneralEnable(uint32_t ui32Interrupt)
+{
+	ASSERT(ui32Interrupt >= 16
+		&& ui32Interrupt < NUM_INTERRUPTS);
+
+	HWREG(NVIC_EN0 + 4 * ((ui32Interrupt - 16) / 32)) =
+            (1 << ((ui32Interrupt - 16) & 31));
+}
+
+inline void
+IntGeneralDisable(uint32_t ui32Interrupt)
+{
+	ASSERT(ui32Interrupt >= 16
+		&& ui32Interrupt < NUM_INTERRUPTS);
+
+	HWREG(NVIC_DIS0 + 4 * ((ui32Interrupt - 16) / 32)) =
+            (1 << ((ui32Interrupt - 16) & 31));
+}
+
+inline bool
+IntGeneralEnabledGet(uint32_t ui32Interrupt)
+{
+	ASSERT(ui32Interrupt >= 16
+		&& ui32Interrupt < NUM_INTERRUPTS);
+
+	return HWREG(NVIC_DIS0 + 4 * ((ui32Interrupt - 16) / 32)) &
+            (1 << ((ui32Interrupt - 16) & 31));
+}
+
+inline bool
+IntGeneralPendGet(uint32_t ui32Interrupt)
+{
+	ASSERT(ui32Interrupt >= 16
+		&& ui32Interrupt < NUM_INTERRUPTS);
+
+	return HWREG(NVIC_PEND0 + 4 * ((ui32Interrupt - 16) / 32)) &
+        (1 << ((ui32Interrupt - 16) & 31));
 }
