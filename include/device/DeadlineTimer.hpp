8fa21846917044eb9adc17fc41fbf9e435816273
akowalew <ram.techen@gmail.com>
2018-05-07 13:35:53 +0200

    device::DeadlineTimer: updated code for handling critical sections

--- include/device/DeadlineTimer.hpp
+++ include/device/DeadlineTimer.hpp
@@ -14,21 +14,26 @@
 
 #include "init.hpp"
 
+#include "device/Peripheral.hpp"
+
 namespace device {
 
 namespace {
 
 } // namespace
 
-template<std::uint32_t TBaseAddress, std::uint32_t TIntNumber>
+template<std::uint32_t TBaseAddress, std::uint32_t TId, std::uint32_t TIntNumber>
 class DeadlineTimer
+	:	public Peripheral<TId>
 {
 public:
 	constexpr static std::uint32_t BaseAddress = TBaseAddress;
-	static_assert(BaseAddress != 0);
+	static_assert(BaseAddress != 0,
+		"Specified BaseAddress is invalid");
 
 	constexpr static std::uint32_t IntNumber = TIntNumber;
-	static_assert(IntNumber < NUM_INTERRUPTS);
+	static_assert(IntNumber < NUM_INTERRUPTS,
+		"Specified IntNumber is invalid");
 
 	constexpr static int Frequency = ClockHz;
 
@@ -39,113 +44,174 @@ public:
 	using EventLoopCtx = embxx::device::context::EventLoop;
 	using InterruptCtx = embxx::device::context::Interrupt;
 
+	/**
+	 * @brief Constructor
+	 * @details [long description]
+	 */
 	DeadlineTimer()
 	{
+		// Interrupts should be locked
+		assert(IntGeneralEnabledGet(IntNumber) == false);
+
+		// Be sure, that during construction Timer is disabled
+		assert(!TimerIsEnabled(BaseAddress, TIMER_BOTH));
+
+		// Be sure, that during construction Timer interrupts are disabled
+		assert(TimerIntEnabledGet(BaseAddress) == 0);
+
 		// Configure timer to work as full-width, one shot
 		MAP_TimerConfigure(BaseAddress, TIMER_CFG_ONE_SHOT);
 
 		// Register interrupt handler and enable it
 		IntRegister(IntNumber, timerISR);
 		IntUserDataSet(IntNumber, static_cast<void*>(this));
-		IntEnable(IntNumber);
 
 		// After construction, timer should be idle
-		assert(!checkWaiting(EventLoopCtx()));
+		assert(!isBusy(InterruptCtx()));
+
+		// Unlock interrupts
+		IntGeneralEnable(IntNumber);
 	}
 
+	/**
+	 * @brief Destructor
+	 * @details [long description]
+	 */
 	~DeadlineTimer()
 	{
-		// Lock guard
-		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
+		// Lock interrupts
+		IntGeneralDisable(IntNumber);
 
-		// During destruction, timer should be idle
-		assert(!checkWaiting(InterruptCtx()));
+		// During destruction, device should be idle
+		assert(!isBusy(InterruptCtx()));
 
 		// Disable interrupt handler and unregister it
-		IntDisable(IntNumber);
 		IntUserDataUnset(IntNumber);
 		IntUnregister(IntNumber);
 	}
 
-	template<typename TFunc>
-	void setTimeoutCallback(TFunc&& func)
+	/**
+	 * @brief Sets timeout handler used in asynchronous operations
+	 * @details [long description]
+	 *
+	 * @param handler [description]
+	 */
+	template<typename THandler>
+	void setTimeoutHandler(THandler&& handler)
 	{
-		// During setting the timeout callback, timer should be idle
-		assert(!checkWaiting(EventLoopCtx()));
+		// Lock interrupts and check, that device should not be busy
+		assert(!isBusy(EventLoopCtx()));
 
-		_timeoutCallback = std::forward<TFunc>(func);
+		// Store provided handler
+		_timeoutHandler = std::forward<THandler>(handler);
 	}
 
+	/**
+	 * @brief Starts asynchronous wait from event loop context
+	 * @details [long description]
+	 *
+	 * @param duration [description]
+	 * @param x [description]
+	 * @tparam TRep [description]
+	 * @tparam TPeriod [description]
+	 */
 	template<typename TRep, typename TPeriod>
 	void startWait(const std::chrono::duration<TRep, TPeriod>& duration,
 		EventLoopCtx)
 	{
-		// Lock guard
-		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
+		// Lock interrupts and check, that device should not be busy
+		assert(!isBusy(EventLoopCtx()));
 
 		// Start wait as in the interrupt context
 		startWait(duration, InterruptCtx());
 	}
 
-	bool isWaiting(EventLoopCtx)
+	/**
+	 * @brief Checks, whether device is busy or not, from event loop context
+	 * @details [long description]
+	 *
+	 * @param  [description]
+	 * @return [description]
+	 */
+	bool isBusy(EventLoopCtx)
 	{
-		// Lock guard
-		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
+		// Lock interrupts
+		IntGeneralDisable(IntNumber);
 
 		// Check status as in the interrupt context
-		const auto waiting = checkWaiting(InterruptCtx());
+		const auto busy = isBusy(InterruptCtx());
 
-		// If it is waiting, reenable interrupts
-		if(waiting)
-		{
-			TimerIntEnable(BaseAddress, TIMER_TIMA_TIMEOUT);
-		}
+		// Unlock interrupts
+		IntGeneralEnable(IntNumber);
 
-		return waiting;
+		return busy;
 	}
 
+	/**
+	 * @brief Cancels asynchronous wait, from event loop context
+	 * @details [long description]
+	 *
+	 * @param  [description]
+	 * @return [description]
+	 */
 	bool cancelWait(EventLoopCtx)
 	{
-		// Lock guard
-		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
+		// Lock interrupts
+		IntGeneralDisable(IntNumber);
 
 		// Cancel wait as in the interrupt context
 		const auto waitCancelled = cancelWait(InterruptCtx());
+
+		// Unlock interrupts
+		IntGeneralEnable(IntNumber);
+
 		return waitCancelled;
 	}
 
 private:
 	template<typename T> using Function = embxx::util::StaticFunction<T, 2 * sizeof(void*)>;
 
-	using TimeoutCallback = Function<void()>;
+	using TimeoutHandler = Function<void()>;
 
+	/**
+	 * @brief Starts asynchronous wait, from interrupt context
+	 * @details [long description]
+	 *
+	 * @param duration [description]
+	 * @param x [description]
+	 * @tparam TRep [description]
+	 * @tparam TPeriod [description]
+	 */
 	template<typename TRep, typename TPeriod>
 	void startWait(const std::chrono::duration<TRep, TPeriod>& duration,
 		InterruptCtx)
 	{
-		// No multiple waits possible
-		assert(!checkWaiting(InterruptCtx()));
+		// Lock interrupts and check, that device should not be busy
+		assert(!isBusy(InterruptCtx()));
 
 		// Calculate load for the timer
 		const auto waitDuration =
 			std::chrono::duration_cast<WaitDuration>(duration);
 		TimerLoadSet(BaseAddress, TIMER_A, waitDuration.count());
 
-		// Assume, that there are no pending interrupts
+		// Assume, that there are no pending interrupts and enable them
 		assert(TimerRawIntStatus(BaseAddress) == 0);
-
-		// Enable timer interrupts, and then launch timer
+		assert(TimerIntEnabledGet(BaseAddress) == 0);
 		TimerIntEnable(BaseAddress, TIMER_TIMA_TIMEOUT);
+
+		// Timer should be now disabled, so enable it
+		assert(!TimerIsEnabled(BaseAddress, TIMER_BOTH));
 		TimerEnable(BaseAddress, TIMER_BOTH);
 	}
 
 	bool cancelWait(InterruptCtx)
 	{
-		if(checkWaiting(InterruptCtx()))
+		if(isBusy(InterruptCtx()))
 		{
 			// Timer was waiting, so disable it, and just to be sure clear interrupts,
 			//  because timeout could occur right after call to `TimerIsEnabled`
 			TimerDisable(BaseAddress, TIMER_A);
+			TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
 			TimerIntClear(BaseAddress, TIMER_TIMA_TIMEOUT);
 
 			// We have successfully cancelled wait
@@ -159,18 +225,7 @@ private:
 		return false;
 	}
 
-	bool checkWaiting(EventLoopCtx)
-	{
-		// Lock guard
-		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
-
-		// Check, if it was waiting, like in the interrupt context
-		const auto waiting = checkWaiting(InterruptCtx());
-
-		return waiting;
-	}
-
-	bool checkWaiting(InterruptCtx)
+	bool isBusy(InterruptCtx)
 	{
 		if(TimerIsEnabled(BaseAddress, TIMER_A))
 		{
@@ -193,30 +248,38 @@ private:
 
 	void handleISR(InterruptCtx)
 	{
+		// Device should be busy since interrupt was fired
+		assert(isBusy(InterruptCtx()));
+
 		// Check, that valid interrupt occured, and clear it
 		assert(TimerMaskedIntStatus(BaseAddress) == TIMER_TIMA_TIMEOUT);
 		TimerIntClear(BaseAddress, TIMER_TIMA_TIMEOUT);
 
-		// Since this timer was in one-shot mode, it should be now disabled
+		// Since this timer was in one-shot mode, it should be now disabled,
+		//  so disable only interrupts
 		assert(!TimerIsEnabled(BaseAddress, TIMER_A));
+		TimerIntDisable(BaseAddress, TIMER_TIMA_TIMEOUT);
 
 		// Wait complete, invoke the callback
-		assert(_timeoutCallback);
-		_timeoutCallback();
+		assert(_timeoutHandler);
+		_timeoutHandler();
 	}
 
 	static void timerISR()
 	{
-		using ThisType = DeadlineTimer<BaseAddress, IntNumber>;
-
+		// Retrieve stored UserData pointer (pointing to this object),
+		// 	and cast it to this object type
+		using ThisType = DeadlineTimer<BaseAddress, TId, IntNumber>;
 		auto userData = IntUserDataGet(IntNumber);
 		auto instance = static_cast<ThisType*>(userData);
 
+		// Obtained pointer should be non-null, so invoke ISR handler
 		assert(instance != nullptr);
 		instance->handleISR(InterruptCtx());
 	}
 
-	TimeoutCallback _timeoutCallback;
+	// Private members
+	TimeoutHandler _timeoutHandler; //< Timeout handler for async operations
 };
 
 } // namespace device
--- include/driver/DeadlineTimer.hpp
+++ include/driver/DeadlineTimer.hpp
@@ -9,40 +9,73 @@
 
 namespace driver {
 
-template<typename TEventLoop, typename TTimerDevice, typename TCallback>
+template<typename TEventLoop, typename TTimerDevice, typename TTimeoutHandler>
 class DeadlineTimer
 {
 public:
 	using EventLoop = TEventLoop;
 	using TimerDevice = TTimerDevice;
-	using Callback = TCallback;
+	using TimeoutHandler = TTimeoutHandler;
 
 	using ErrorCode = embxx::error::ErrorCode;
 
-	static_assert(std::is_same_v<typename Callback::result_type, void>,
+	static_assert(std::is_same_v<typename TimeoutHandler::result_type, void>,
 		"Result type of callback must be 'void'");
-	// static_assert(std::is_same_v<Callback::argument_type, ErrorCode>,
+	// static_assert(std::is_same_v<TimeoutHandler::argument_type, ErrorCode>,
 		// "Argument type of callback must be 'ErrorCode'");
 
+	/**
+	 * @brief Constructor
+	 * @details [long description]
+	 *
+	 * @param eventLoop [description]
+	 * @param timerDevice [description]
+	 * @param e [description]
+	 */
 	DeadlineTimer(EventLoop& eventLoop, TimerDevice& timerDevice)
 		:	_eventLoop(eventLoop),
 			_timerDevice(timerDevice)
 	{
-		_timerDevice.setTimeoutCallback(
-			[this]() { timerTimeout(InterruptCtx()); });
+		_timerDevice.setTimeoutHandler(
+			[this]()
+			{
+				timerTimeout(InterruptCtx());
+			});
+
+		// Postcondition: driver should not be busy
+		assert(!isBusy());
 	}
 
-	template<typename TRep, typename TPeriod, typename TFunc>
+	/**
+	 * @brief Starts asynchronous wait
+	 * @details [long description]
+	 *
+	 * @param waitDuration [description]
+	 * @param handler [description]
+	 * @tparam TRep [description]
+	 * @tparam TPeriod [description]
+	 */
+	template<typename TRep, typename TPeriod, typename THandler>
 	void asyncWait(std::chrono::duration<TRep, TPeriod> waitDuration,
-		TFunc&& func)
+		THandler&& handler)
 	{
 		// No multiple waits possible
-		assert(!_timerDevice.isWaiting(EventLoopCtx()));
+		assert(!_timerDevice.isBusy(EventLoopCtx()));
 
-		_callback = std::forward<TFunc>(func);
+		// Store provided handler
+		_timeoutHandler = std::forward<THandler>(handler);
+
+		// Start wait from event loop context
 		_timerDevice.startWait(waitDuration, EventLoopCtx());
 	}
 
+	/**
+	 * @brief Cancels asynchronous wait
+	 * @details [long description]
+	 *
+	 * @param cancelled [description]
+	 * @return [description]
+	 */
 	bool cancel()
 	{
 		if(const auto cancelled = _timerDevice.cancelWait(EventLoopCtx()); cancelled)
@@ -51,8 +84,9 @@ public:
 			const auto postSuccess = _eventLoop.post(
 				[this]()
 				{
-					assert(_callback);
-					_callback(ErrorCode::Aborted);
+					// Timeout handler should be non-null, so invoke it
+					assert(_timeoutHandler);
+					_timeoutHandler(ErrorCode::Aborted);
 				});
 			assert(postSuccess);
 			static_cast<void>(postSuccess);
@@ -64,29 +98,43 @@ public:
 		return false;
 	}
 
-	bool isWaiting()
+	/**
+	 * @brief Checks, if driver is busy or not
+	 * @details [long description]
+	 *
+	 * @param _timerDevice [description]
+	 * @return [description]
+	 */
+	bool isBusy()
 	{
-		return _timerDevice.isWaiting(EventLoopCtx());
+		return _timerDevice.isBusy(EventLoopCtx());
 	}
 
 private:
 	using EventLoopCtx = embxx::device::context::EventLoop;
 	using InterruptCtx = embxx::device::context::Interrupt;
 
+	/**
+	 * @brief Timer device timeout handler. Should be invoked from interrupt
+	 * @details [long description]
+	 *
+	 * @param  [description]
+	 */
 	void timerTimeout(InterruptCtx)
 	{
 		// Timer timeout ocurred. Post callback with 'Success' status
 		const auto postSuccess = _eventLoop.postInterruptCtx(
 			[this]()
 			{
-				assert(_callback);
-				_callback(ErrorCode::Success);
+				// Timeout handler should be non-null, so invoke it
+				assert(_timeoutHandler);
+				_timeoutHandler(ErrorCode::Success);
 			});
 		assert(postSuccess);
 		static_cast<void>(postSuccess);
 	}
 
-	Callback _callback;
+	TimeoutHandler _timeoutHandler;
 	EventLoop& _eventLoop;
 	TimerDevice& _timerDevice;
 };
